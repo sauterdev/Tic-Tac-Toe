95c190a2d8494e10669e5d27adc4512d13cf76dd
Chris Sauter <sauterdev@gmail.com>
2023-01-26 21:21:57 -0500

    fixed turn counter

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ class Gameboard {
       board.appendChild(newSquare);
       this.gameGrid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
       this.currentPlayer = this.player1;
+      this.turnCounter = 0;
     }
   };
 
